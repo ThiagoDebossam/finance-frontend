023fe0fbe7ecfead1a4322950eb4b544fdf529cd
ThiagoDebossam <thiagodebossam8@gmail.com>
2023-05-30 00:21:24 -0300

    forgot password

--- src/app/login/Login.vue
+++ src/app/login/Login.vue
@@ -5,7 +5,7 @@
                 <v-card>
                     <v-card-title class="justify-center">Finanças</v-card-title>
                     <v-card-text>
-                        <v-form lazy-validation v-model="valid" @submit.prevent="login" ref="form">
+                        <v-form lazy-validation v-model="valid" @submit.prevent="!processForgotPassword ? login() : forgotPassword()" ref="form">
                             <v-text-field 
                                 placeholder="Digite seu e-mail"
                                 class="required-field"
@@ -15,7 +15,8 @@
                                 ref="email"
                                 :rules="required"
                                 ></v-text-field>
-                            <v-text-field 
+                            <v-text-field
+                                v-if="!processForgotPassword"
                                 placeholder="Digite sua senha" 
                                 class="required-field"
                                 outlined
@@ -26,10 +27,27 @@
                                 v-model="payload.password"
                                 :type="visibility ? 'text' : 'password'"
                                 :rules="required"></v-text-field>
-                            <v-btn :disabled="!valid" class="primary" type="submit" block>Entrar</v-btn>
+                            <v-btn :loading="loading" v-if="!processForgotPassword" :disabled="!valid" class="primary" type="submit" block>
+                                Entrar
+                                <template v-slot:loader>
+                                    <span class="custom-loader">
+                                    <v-icon light>mdi-cached</v-icon>
+                                    </span>
+                                </template>
+                            </v-btn>
+                            <v-btn :loading="loading" v-else :disabled="!valid" class="primary" type="submit" block>
+                                Enviar e-mail
+                                <template v-slot:loader>
+                                    <span class="custom-loader">
+                                    <v-icon light>mdi-cached</v-icon>
+                                    </span>
+                                </template>
+                            </v-btn>
                         </v-form>
                     </v-card-text>
-                    <v-card-actions class="mx-3">
+                    <v-card-actions class="mx-3 d-flex justify-space-between">
+                        <a v-if="processForgotPassword" @click="processForgotPassword = false" class="link-sign-up">Voltar para o login</a>
+                        <a v-else @click="forgotPassword" class="link-sign-up">Esqueci minha senha</a>
                         <a @click="$router.push({name: 'user.create'})" class="link-sign-up">Não possui cadastro ?</a>
                     </v-card-actions>
                 </v-card>
@@ -45,7 +63,9 @@ export default {
         return {
             payload: {},
             valid: true,
-            visibility: false
+            visibility: false,
+            loading: false,
+            processForgotPassword: false
         }
     },
     mounted () {
@@ -54,11 +74,13 @@ export default {
         this.$refs.email.focus()
     },
     methods: {
-        login () {
+        async login () {
             if (this.$refs.form.validate()) {
-                this.$http.post('sign-in', this.payload)
+                this.loading = true
+                await this.$http.post('sign-in', this.payload)
                     .then(this.afterLogin)
                     .catch(this.$throwException)
+                this.loading = false
             } else {
                 this.$fnError('Formulário incompleto')
             }
@@ -68,6 +90,39 @@ export default {
             this.$nextTick(() => {
                 this.$router.go()
             })
+        },
+        forgotPassword () {
+            if (!this.payload.email) {
+                this.$fnInfo('Digite seu email')
+                this.$refs.email.focus()
+                this.processForgotPassword = true
+                return false
+            }
+            const call = async () => {
+                this.loading = true
+                await this.$http.post(`forgot-password`, {email: this.payload.email})
+                    .then(this.afterForgotPassword)
+                    .catch(this.$throwException)
+                this.loading = false
+            }
+            if (!this.processForgotPassword) {
+                this.$askBefore(call, () => {
+                    if (!this.processForgotPassword) {
+                        this.processForgotPassword = true
+                        this.$nextTick(() => {
+                            this.$refs.form.reset()
+                        })
+                    }
+                }, 'Atenção', `Deseja solicitar alteração de senha para o e-mail '${this.payload.email}' ?`)
+            } else {
+                call()
+            }
+        },
+        afterForgotPassword ({ data }) {
+            this.loading = false
+            if (this.processForgotPassword) this.processForgotPassword = false
+            this.$fnSuccess('E-mail enviado')
+            console.log(data)
         }
     }
 }
--- src/install/mixin/index.js
+++ src/install/mixin/index.js
@@ -35,6 +35,7 @@ Vue.mixin({
             this.$toast('error', msg)
         },
         $throwException(error) {
+            console.error(error)
             if (!error) {
                 this.$fnError('Erro interno')
                 return false
@@ -43,16 +44,17 @@ Vue.mixin({
                 const { response } = error
                 if (response.status === 401) {
                     this.setExpiredSession(true)
-                }
-                const { data } = response.data
-                if (data) {
+                } else if (response.status === 404) {
+                    this.$fnError('Rota não encontrada')
+                } else if (response.data && response.data.data) {
+                    const { data } = response.data
                     const errors = Object.keys(data)
                     this.$fnError(data[errors[0]])
                 } else if (response.data && response.data.msg) {
                     this.$fnError(response.data.msg)
                 }
             } catch (e) {
-                this.$fnError(error)
+                this.$fnError('Erro interno!')
             }
         },
         $fnSuccess(msg) {
@@ -60,6 +62,44 @@ Vue.mixin({
         },
         $fnInfo(msg) {
             this.$toast('info', msg)
+        },
+        $message(message = 'OK', type = 'warning', showTitle = true) {
+            Swal(showTitle ? 'Chronos+' : undefined, message, type || '')
+        },
+        $askBefore(
+            fnConfirmation,
+            fnCancel,
+            title,
+            text,
+            confirmButtonText = 'Sim',
+            cancelButtonText = 'Não',
+            icon = 'warning') {
+            Swal.fire({
+                title,
+                text,
+                icon,
+                confirmButtonText,
+                cancelButtonText,
+                showCancelButton: true,
+                cancelButtonColor: '#c4c4c4',
+                confirmButtonColor: '#3085d6',
+                reverseButtons: true,
+                showCloseButton: false,
+                customClass: {
+                    popup: 'swal-popup',
+                    content: 'content-popup',
+                    actions: 'actions-custom',
+                    title: 'swal2-title ',
+                    htmlContainer: 'swal2-htmlContainer'
+                }
+
+            }).then((result) => {
+                if (result.value) {
+                    fnConfirmation()
+                } else {
+                    if (typeof fnCancel === 'function') fnCancel()
+                }
+            })
         }
     }
 })
