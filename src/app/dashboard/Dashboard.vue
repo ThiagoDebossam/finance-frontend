a97bbec8952628618d3caa30c58df8dd32caec7e
ThiagoDebossam <thiagodebossam8@gmail.com>
2023-06-09 21:42:21 -0300

    init dashboard

--- src/app/dashboard/Dashboard.vue
+++ src/app/dashboard/Dashboard.vue
@@ -1,10 +1,42 @@
 <template>
     <div>
-        Oi
+        <v-container fluid>
+            <v-flex v-if="dontHaveAccounts" text-xs-center class="d-flex column no-has-account">
+                <div>Você ainda não possui nenhuma conta. Vamos criar uma ?</div>
+                <v-btn color="#0c6c00" class="white--text mt-2">Criar conta</v-btn>
+            </v-flex>
+            <div v-else>
+                oi
+            </div>
+        </v-container>
     </div>
 </template>
 <script>
 export default {
-    name: 'Dashboard'
+    name: 'Dashboard',
+    data () {
+        return {
+            accounts: []
+        }
+    },
+    mounted () {
+        this.getAccounts()
+    },
+    computed: {
+        dontHaveAccounts () {
+            return this.accounts.length === 0
+        }
+    },
+    methods: {
+        getAccounts () {
+            this.$http('/account')
+                .then(this.afterGetAccounts)
+                .catch(this.$throwException)
+        },
+        afterGetAccounts ({ data }) {
+            this.accounts = data
+        }
+    }
 }
 </script>
+<style lang="sass" scoped >@import "./Dashboard.scss"</style>
